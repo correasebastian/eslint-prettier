d718d962830619ec07dbe6a6feaa1484e81a218d
sebastian correa <scorrea1@wsgc.com>
2018-03-20 09:50:33 -0400

    initial commit, only using eslint-config-prettier

---
+++ src/one.js
@@ -0,0 +1,17 @@
+function name    (params) {
+    switch (params) {
+        case 'hi'    :
+        console.log('npm install --save-dev eslint-config-prettier npm install --save-dev eslint-config-prettier npm install --save-dev eslint-config-prettier');
+            break;
+    
+        default:
+            break;
+    }
+}
+
+const value = 'hi everyone'.trim().split("\n")[0].toLowerCase();
+
+var hello = "world"
+[1, 2, 3].forEach(addNumber)
+
+function addNumber(number) { return number +1  }
